584d05bdb73172cd1b36c9b4a4e1a526f5a14549
john-c-freeman <130423057+john-c-freeman@users.noreply.github.com>
2023-04-17 12:46:15 -0600

    Update HandPanel.java

--- Project_CardGame/src/gui/HandPanel.java
+++ Project_CardGame/src/gui/HandPanel.java
@@ -4,12 +4,18 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
+
+import game.Alliance;
+import game.Player;
+import game.card.Bear;
+import game.card.Card;
 
 import java.awt.event.ActionListener;
 import java.util.*;
@@ -20,18 +26,19 @@ import java.awt.GridBagConstraints;
 @SuppressWarnings("serial")
 public class HandPanel extends JPanel {
 	private List<JButton> listHand = new ArrayList<>();
-	private List<ImageIcon> listHandIcon = new ArrayList<ImageIcon>();
+	//private List<ImageIcon> listHandIcon = new ArrayList<ImageIcon>();
 
 	//private ImageIcon cardSelectedIcon = new ImageIcon(); // 
 
-	private String[] files = { "/gui/Resources/test1.png", "/gui/Resources/test2.png", "/gui/Resources/test3.png",
-			"/gui/Resources/test1.png", "/gui/Resources/test2.png", "/gui/Resources/test3.png" };
+	//private String[] files = { "/gui/Resources/test1.png", "/gui/Resources/test2.png", "/gui/Resources/test3.png",
+	//		"/gui/Resources/test1.png", "/gui/Resources/test2.png", "/gui/Resources/test3.png" };
 
-	private int index = 0;
+	//private int index = 0;
 	private int totalSlotsAvailable = 7; // only needed if we want to restrict the number of cards a player can hold
 	public int currentSlotInHand = 0; // the package knows how many cards are in the players hands
 	public static List<Boolean> listBattleAreaSlotAvailable = new ArrayList<>(Arrays.asList(true, true, true, true));; // Check if there is a card already on the board for that index
 	public static boolean hasPlayed = false; // Once the player places a card, they can no longer place a card until the next turn
+	public int cardCounter = 0;
 	/**
 	 * 
 	 * Creates the panel. (Constructor)
@@ -62,31 +69,42 @@ public class HandPanel extends JPanel {
 	 */
 	public JButton create_Card() {
 		JButton btnNewButton = new JButton();
-		btnNewButton.setPreferredSize(new Dimension(100, 100)); // set's the button to be a reasonable size
-		listHand.add(btnNewButton); // Adds a new JButton (Card) to the Hand List
-		add(listHand.get(listHand.size() - 1)); // Add that same new Jbutton (Card) to the JPanel
+		Card card = GuiBoard.player.drawCard();
 		
-		listHand.get(listHand.size() - 1).addActionListener(new ActionListener() { // Once the card is chosen
+		btnNewButton.setText(card.toString() + " | cost: " + Integer.toString(card.getMana()));
+		btnNewButton.setPreferredSize(new Dimension(150, 150)); // set's the button to be a reasonable size
+		cardCounter++;
+		listHand.add(btnNewButton); // Adds a new JButton (Card) to the Hand List
+		add(btnNewButton); // Add that same new Jbutton (Card) to the JPanel
+		
+		btnNewButton.addActionListener(new ActionListener() { // Once the card is chosen
 			public void actionPerformed(ActionEvent e) {
-				handToBattleArea(0); // Play the card to the Battle Area index 0 
-				handToBattleArea(1);
-				handToBattleArea(2);
-				handToBattleArea(3);
+				if ( GuiBoard.player.getMana() >= card.getMana()) {
+					handToBattleArea(0); // Play the card to the Battle Area index 0 
+					handToBattleArea(1);
+					handToBattleArea(2);
+					handToBattleArea(3);
+					
+					btnNewButton.setVisible(false);; // Hides the card so it no longer appears in players hand
+					listHand.remove(btnNewButton);
+				}
+	
 			}
 
 			/**
-			 * 
 			 * @param areaIndex
+			 * @param actionListener 
 			 */
 			private void handToBattleArea(int areaIndex) {
 				if (hasPlayed == false) {
-					if (BattleAreaPanel.battleAreaButtons.get(areaIndex).isSelected() && listBattleAreaSlotAvailable.get(areaIndex) == true) {
-						BattleAreaPanel.battleAreaButtons.get(areaIndex).setIcon(listHandIcon.get(listHand.size() - 1));   //ERROR it always takes the last card pulled
-						listHand.get(listHand.size() - 1).setVisible(false);; // Hides the card so it no longer appears in players hand
-						
+					if (BattleAreaPanel.battleAreaButtons.get(areaIndex).isSelected() 
+							&& listBattleAreaSlotAvailable.get(areaIndex) == true ) {
+						BattleAreaPanel.battleAreaButtons.get(areaIndex).setText(card.toString());  
 						hasPlayed = true;
 						BattleAreaPanel.battleAreaButtons.get(areaIndex).setSelected(false);
 						listBattleAreaSlotAvailable.set(areaIndex, false);
+					    GuiBoard.player.setMana(GuiBoard.player.getMana()-card.getMana());
+
 					}
 				}
 			}
@@ -99,11 +117,13 @@ public class HandPanel extends JPanel {
 	 * soas to know how many cards are in the players hand. 
 	 */
 	public void addCardToHand() {
-		listHandIcon.add(new ImageIcon(HandPanel.class.getResource(files[index]))); // adds new image to list Icon
+		//listHandIcon.add(new ImageIcon(HandPanel.class.getResource(files[index]))); // adds new image to list Icon
 		create_Card(); // Creates a new card
-		listHand.get(currentSlotInHand).setIcon(listHandIcon.get(currentSlotInHand)); // sets the icon to the new card
+		revalidate();
+
+		//listHand.get(currentSlotInHand).setIcon(listHandIcon.get(currentSlotInHand)); // sets the icon to the new card
 
 		currentSlotInHand = ++currentSlotInHand % totalSlotsAvailable; // allows the program to keep trying regardless of how small our minimum slots allowed value is. 
-		index = ++index % files.length; // allows the program to keep trying regardless if the deck is out of cards. 
+		//index = ++index % files.length; // allows the program to keep trying regardless if the deck is out of cards. 
 	}
 }
