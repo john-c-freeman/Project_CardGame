8abd48814b1720a97066cd5cc3f7652bb8c2d34f
john-c-freeman <130423057+john-c-freeman@users.noreply.github.com>
2023-04-17 12:44:15 -0600

    Update BattleAreaPanel.java

--- Project_CardGame/src/gui/BattleAreaPanel.java
+++ Project_CardGame/src/gui/BattleAreaPanel.java
@@ -12,43 +12,37 @@ import java.awt.event.ActionListener;
 import java.util.*;
 import java.awt.event.ActionEvent;
 import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class BattleAreaPanel extends JPanel {
 
 	public static List<JToggleButton> battleAreaButtons = new ArrayList<>();
-
+	private static List<JLabel> ComputerSetToPlayList = new ArrayList<>();
+	private static List<JLabel> ComputerInPlayList = new ArrayList<>();
 	/**
 	 * Create the panel.
 	 */
 	public BattleAreaPanel() {
-		JPanel panel = new JPanel();  
 		int tableWidth = 4;
 		
 		setLayout(new GridLayout(3, 4, 50, 50));
+		for (int i = 0; i < tableWidth; i++) {
+			ComputerSetToPlayList.add(new JLabel("",SwingConstants.CENTER));
+			ComputerInPlayList.add(new JLabel());
+		}
+		ComputerSetToPlayList.forEach(x -> x.setBorder(new LineBorder(new Color(0, 0, 0))));
+		ComputerInPlayList.forEach(x -> x.setBorder(new LineBorder(new Color(0, 0, 0))));
+		ComputerSetToPlayList.forEach(x -> x.setPreferredSize(new Dimension(100, 100)));
+		ComputerInPlayList.forEach(x -> x.setPreferredSize(new Dimension(100, 100)));
+
+		ComputerSetToPlayList.forEach(x -> add(x));
+		ComputerInPlayList.forEach(x -> add(x));
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		add(lblNewLabel);
+		ComputerSetToPlayList.get(0).setText("test");
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		add(lblNewLabel_6);
-		
-		JLabel lblNewLabel_9 = new JLabel("New label");
-		add(lblNewLabel_9);
-		
-		JLabel lblNewLabel_7 = new JLabel("New label");
-		add(lblNewLabel_7);
-		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		add(lblNewLabel_4);
-		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		add(lblNewLabel_1);
 		
 		for (int i = 0; i < tableWidth; i++) { // Initializes the 4 BattleArea slots to the list
 			battleAreaButtons.add(new JToggleButton());
